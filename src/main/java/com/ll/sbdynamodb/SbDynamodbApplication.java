c77d1da8c36b012df6dc6c9ce0006ec5311d1729
sik2 <bickwang123@gmail.com>
2025-02-28 17:29:25 +0900

    init

---
+++ src/main/java/com/ll/sbdynamodb/SbDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbdynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDynamodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDynamodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbdynamodb/SbDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDynamodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
